42853c3f139fd8db5de922dc07f7e98270435205
rakhmatnw <rakhmatnashrul@gmail.com>
2019-03-15 22:47:27 +0700

    [rakhmatnw] - added sass reset & variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+        </div>
+      </Router>
     );
   }
 }
